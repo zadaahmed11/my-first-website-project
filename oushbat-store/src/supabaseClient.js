851ae4458fd8ca7e93398da8eba14e812a493dfb
zadaahmed11 <zadaahmed1510@gmail.com>
2026-08-19 01:24:54 +0300

    Modification the supabaseUrl

--- oushbat-store/src/supabaseClient.js
+++ oushbat-store/src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://supabase.co';
+const supabaseUrl = 'https://bdzdhepkknezyzkbhztk.supabase.co';
 const supabaseAnonKey = 'sb_publishable_r5uQUwf3UG0ABsmTXRv33Q_m4SPocTt';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
