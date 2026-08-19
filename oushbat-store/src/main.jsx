de791e291106b5c6b243dccaa836948b01801e44
zadaahmed11 <zadaahmed1510@gmail.com>
2026-08-19 11:09:41 +0300

    update

--- oushbat-store/src/main.jsx
+++ oushbat-store/src/main.jsx
@@ -1,4 +1,12 @@
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from 'react';
+import { createRoot } from 'react-dom/client'; 
+import App from './App.jsx';
+import './index.css'; 
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
